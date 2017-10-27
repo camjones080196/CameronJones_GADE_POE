94fc0013f573dd7d276913e68029383b804c239a
Cam Jones <camo080196@gmail.com>
2017-10-27 10:39:08 +0200

    4th Commit
    4th Commit

---
+++ CameronJones_GADE_POE/Assets/Scripts/Building.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameronJones_GADE1B_A2
+{
+    abstract class Building
+    {
+        //**************************************************************************************************************** Variables *************************************************************************************************************************************
+        private int xpos;
+        private int ypos;
+        private int health;
+        private string faction;
+        private char symbol;
+        private char buildingSymbol;
+
+        //**************************************************************************************************************** G&S's *************************************************************************************************************************************
+
+        protected int Xpos { get => xpos; set => xpos = value; }
+        protected int Ypos { get => ypos; set => ypos = value; }
+        protected int Health { get => health; set => health = value; }
+        protected string Faction { get => faction; set => faction = value; }
+        protected char Symbol { get => symbol; set => symbol = value; }
+        public char Buildingsymbol { get => buildingSymbol; set => buildingSymbol = value; }
+
+        //**************************************************************************************************************** Constructor & Destructor *************************************************************************************************************************************
+
+        public Building()
+        {
+            //code
+        }
+
+        ~Building()
+        {
+            //code
+        }
+
+        //**************************************************************************************************************** Methods *************************************************************************************************************************************
+
+        public abstract override string ToString();
+       
+
+    }
+}
---
+++ CameronJones_GADE_POE/Assets/Scripts/ResourceBuilding.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameronJones_GADE1B_A2
+{
+    class ResourceBuilding : Building
+    {
+        //**************************************************************************************************************** Variables *************************************************************************************************************************************
+
+        char resourceType;
+        int resourcesPerGameTick;
+        int resourcesRemaining = 50;
+        int xPos, yPos;
+        Random r = new Random();
+
+
+        //**************************************************************************************************************** G&S's *************************************************************************************************************************************
+
+        public char ResourceType { get => resourceType; set => resourceType = value; }
+        public int ResourcesPerGameTick { get => resourcesPerGameTick; set => resourcesPerGameTick = value; }
+        public int ResourcesRemaining { get => resourcesRemaining; set => resourcesRemaining = value; }
+        public int XPos { get => xPos; set => xPos = value; }
+        public int YPos { get => yPos; set => yPos = value; }
+
+        //**************************************************************************************************************** Constructor & Destructor *************************************************************************************************************************************
+
+        public ResourceBuilding()
+        {
+            XPos = 0;
+            YPos = 0;
+            Buildingsymbol = '#';
+        }
+
+        ~ResourceBuilding()
+        {
+
+        }
+
+        //**************************************************************************************************************** Methods *************************************************************************************************************************************
+
+        public override string ToString()
+        {
+            return "Resource type: " + resourceType;
+        }
+
+        public void GenerateResource()
+        {
+
+            resourcesPerGameTick = r.Next(1, 3);
+            resourcesRemaining = resourcesRemaining - resourcesPerGameTick;
+        }
+    }
+}
